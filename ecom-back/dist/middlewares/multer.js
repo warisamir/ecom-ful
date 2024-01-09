6efbca737889bb2e88ce3e86528df45f05f3d994
warisamir <warisamir1918@gmail.com>
2024-01-09 18:57:00 +0530

    finish multer image uploads

--- ecom-back/dist/middlewares/multer.js
+++ ecom-back/dist/middlewares/multer.js
@@ -1,4 +1,12 @@
 import multer from "multer";
 multer().single("file");
-const storage = multer.diskStorage({});
+//storing it in the harddisk not in server so that after server restart file remain
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
 export const singleupload = multer({ storage }).single("photo");
--- ecom-back/src/middlewares/multer.ts
+++ ecom-back/src/middlewares/multer.ts
@@ -1,5 +1,13 @@
 import multer from "multer";
 multer().single("file");
+//storing it in the harddisk not in server so that after server restart file remain
 
-const storage=multer.diskStorage({});
-export const singleupload=multer({storage}).single("photo")
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"uploads");
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname);
+    }
+});
+export const singleupload=multer({storage}).single("photo");
